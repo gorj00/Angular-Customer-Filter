3fab21ecee057239d848fe191bd75a4c83dab6df
Jan Gorol <os.jangorol@gmail.com>
2022-12-18 10:36:58 +0100

    add step button

--- src/app/modules/primeng.module.ts
+++ src/app/modules/primeng.module.ts
@@ -5,7 +5,9 @@ import { ButtonModule } from 'primeng/button';
 import { ListboxModule } from 'primeng/listbox';
 import { DropdownModule}  from 'primeng/dropdown';
 import { TabMenuModule } from 'primeng/tabmenu';
-import {InputTextModule} from 'primeng/inputtext';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   imports: [CommonModule],
@@ -16,6 +18,8 @@ import {InputTextModule} from 'primeng/inputtext';
     DropdownModule,
     TabMenuModule,
     InputTextModule,
+    CardModule,
+    DividerModule,
   ],
 })
 export class PrimeNgModule {}
